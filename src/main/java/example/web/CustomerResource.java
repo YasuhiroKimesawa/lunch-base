c4bfdb515b4e08469fde58f557e5f81f79aa9a17
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2016-05-07 23:39:37 +0900

    post追加

--- src/main/java/example/web/CustomerResource.java
+++ src/main/java/example/web/CustomerResource.java
@@ -3,6 +3,7 @@ package example.web;
 import example.model.customer.Customer;
 import example.model.customer.CustomerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,12 @@ public class CustomerResource
     {
         Customer customer = customerFactory.create( 1, "田中", "達也" );
 
+        return customer;
+    }
+
+    @RequestMapping( value = "", method = RequestMethod.POST )
+    public Customer register( @RequestBody Customer customer )
+    {
         return customer;
     }
 }
