0e4c7dc9efbd87b62bace3ba7abc0ad849a7602a
abehazuki <abe_hazuki@aratana.jp>
2016-08-30 15:19:40 +0900

    Userディレクトリの追加

---
+++ src/main/java/example/model/User/EmployeePassword.java
@@ -0,0 +1,7 @@
+package example.model.User;
+
+/**
+ * Created by mac48 on 2016/08/10.
+ */
+public class EmployeePassword {
+}
---
+++ src/main/java/example/model/User/User.java
@@ -0,0 +1,9 @@
+package example.model.User;
+
+/**
+ * Created by mac48 on 2016/08/10.
+ */
+public class User {
+    private EmployeeNumber employeeNumber;
+    private EmployeePassword employeePassword;
+}
